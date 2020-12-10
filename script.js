edd481d718c7f7914f143a3246b33ecb94512ffd
shaneocon <shane.pat.oconnell@gmail.com>
2020-12-10 06:53:30 -0800

    added variables of characters

--- script.js
+++ script.js
@@ -1,52 +1,8 @@
-// Assignment Code
+
 var generateBtn = document.querySelector("#generate");
 var pwTextArea = document.querySelector("#password")
-var lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz".split('');
-var uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUZWXYZ".split('');
-var numericalCharacters = "0123456789".split('');
-var specialCharacters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split('');
+var lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUZWXYZ";
+var numericalCharacters = "0123456789";
+var specialCharacters = "!#$%&'()*+-./:;<=>?@[\^_`{|}~";
 
-
-function generatePassword() {
-
-    return "password";
-}
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-var charType = prompt("Enter a character type: special, numeric, uppercase, lowercase.");
-
-//generate password
-function generatePassword() {
-  //evaluate character type
-  var charSet = "";
-  var charTypeLower = charType.toLowerCase();
-  if( charTypeLower === "lowercase" ) {
-    charSet = "abcdefghijklmnopqrstuvwxyz";
-  } else if( charTypeLower === "uppercase" ) {
-    charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  } else if( charTypeLower === "numeric" ) {
-    charSet = "0123456789";
-  } else if( charTypeLower === "special" ) {
-    charSet = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-  } 
-  //return value
-  var retVal = "";
-  for (var i = 0; i < length; i++) {
-    //picks a character within charSet at index of random number
-    retVal += charSet.charAt(Math.floor(Math.random() * charSet.length));
-  }
-  return retVal;
-}
-alert(generatePassword());
