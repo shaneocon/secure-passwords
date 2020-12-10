a001a50271dac0696e06b00337affeffe5f726d9
shaneocon <shane.pat.oconnell@gmail.com>
2020-12-09 21:39:51 -0800

    added character variables

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var pwTextArea = document.querySelector("#password")
+var lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz".split('');
+var uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUZWXYZ".split('');
+var numericalCharacters = "0123456789".split('');
+var specialCharacters = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split('');
+
+
 function generatePassword() {
+
     return "password";
 }
 
@@ -16,3 +23,30 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+var charType = prompt("Enter a character type: special, numeric, uppercase, lowercase.");
+
+//generate password
+function generatePassword() {
+  //evaluate character type
+  var charSet = "";
+  var charTypeLower = charType.toLowerCase();
+  if( charTypeLower === "lowercase" ) {
+    charSet = "abcdefghijklmnopqrstuvwxyz";
+  } else if( charTypeLower === "uppercase" ) {
+    charSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  } else if( charTypeLower === "numeric" ) {
+    charSet = "0123456789";
+  } else if( charTypeLower === "special" ) {
+    charSet = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  } 
+  //return value
+  var retVal = "";
+  for (var i = 0; i < length; i++) {
+    //picks a character within charSet at index of random number
+    retVal += charSet.charAt(Math.floor(Math.random() * charSet.length));
+  }
+  return retVal;
+}
+alert(generatePassword());
