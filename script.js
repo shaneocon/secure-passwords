c3fb4a9bd4fa1fa5b7d6a5023b4e67b669fb2295
shaneocon <shane.pat.oconnell@gmail.com>
2020-12-10 08:57:02 -0800

    lowercase confirm

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 var generateBtn = document.querySelector("#generate");
 var pwTextArea = document.querySelector("#password")
+
 var lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
 var uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUZWXYZ";
 var numericalCharacters = "0123456789";
@@ -11,19 +12,22 @@ generateBtn.addEventListener("click", generatePassword);
 function generatePassword() {
 
     var newPassword = "";
-    var passwordCharset = "";
+    var passwordCharSet = "";
   
-    var passwordLengthUser = prompt("How many characters would you like your password to be?");
+    var passwordLengthUser = prompt("How many characters would you like your password to be? Password must be between 8-128 characters.");
   
     if ((passwordLengthUser < 8) , (passwordLengthUser > 128) , isNaN(passwordLengthUser)) {
         alert("Please enter a number between 8 and 128!");
         return;
     }
     
-    var lowercaseCharactersInput = confirm("Sprinkle in some lowercase characters?");
- 
-    if (lowercaseCharactersInput) {
-        passwordCharset += lowercaseCharacters;
+    var lowercaseCharactersChoice = confirm("Sprinkle in some lowercase characters?");
+    
+    if (lowercaseCharactersChoice) {
+       passwordCharSet += lowercaseCharacters;
     }
- 
+  
+
+
+
 }
