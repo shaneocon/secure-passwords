e0abce7ba70600f0f1c2188a49a2ef51e0ff328f
shaneocon <shane.pat.oconnell@gmail.com>
2020-12-10 07:42:41 -0800

    added first prompt determining password length

--- script.js
+++ script.js
@@ -9,5 +9,15 @@ var specialCharacters = "!#$%&'()*+-./:;<=>?@[\^_`{|}~";
 generateBtn.addEventListener("click", generatePassword);
 
 function generatePassword() {
-    
+
+    var newPassword = "";
+    var passwordCharset = "";
+  
+    var passwordLengthUser = prompt("How many characters would you like your password to be?");
+  
+    if (Number(passwordLengthUser < 8) , Number(passwordLengthUser > 128) , isNaN(passwordLengthUser)) {
+        alert("Please enter a number between 8 and 128!");
+        return;
+    }
+ 
 }
