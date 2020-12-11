6888256cf60dd9df854792020c0ee8ae56e7cbd3
shaneocon <shane.pat.oconnell@gmail.com>
2020-12-10 16:43:09 -0800

    everything but the kitchen sink (no code is generating- yet

--- script.js
+++ script.js
@@ -1,42 +1,64 @@
-
 var generateBtn = document.querySelector("#generate");
-var pwTextArea = document.querySelector("#password")
 
-var lowercaseCharacters = "abcdefghijklmnopqrstuvwxyz";
-var uppercaseCharacters = "ABCDEFGHIJKLMNOPQRSTUZWXYZ";
-var numericalCharacters = "0123456789";
-var specialCharacters = "!#$%&'()*+-./:;<=>?@[\^_`{|}~";
 
-generateBtn.addEventListener("click", generatePassword);
+var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUZWXYZ";
+var numericalChar = "0123456789";
+var specialChar = "!#$%&'()*+-./:;<=>?@[\^_`{|}~";
 
 function generatePassword() {
 
-    var newPassword = "";
-    var passwordCharSet = "";
-  
+    var newPw = "";
+    var passwordChar = "";
+    //    creates user prompt to select password length
     var passwordLengthUser = prompt("How many characters would you like your password to be? Password must be between 8-128 characters.");
-  
-    if ((passwordLengthUser < 8) , (passwordLengthUser > 128) , isNaN(passwordLengthUser)) {
-        alert("Please enter a number between 8 and 128!");
-        return;
+    passwordLengthUser = parseInt(passwordLengthUser);
+
+    if (passwordLengthUser < 8) {
+        alert("Password must have more than 7 characters!");
+        return "";
     }
-    
+
+    if (passwordLengthUser > 128) {
+        alert("Password much not have more than 128 characters!");
+        return ""
+    }
+
+    //  creates confirm boolean for lowercase "yes or no"
     var lowercaseCharactersChoice = confirm("Sprinkle in some lowercase characters?");
-    
+
     if (lowercaseCharactersChoice) {
-       passwordCharSet += lowercaseCharacters;
+        passwordChar += lowercaseChar;
     }
-  
-    var uppercaseCharactersChoice = confirm("How about a few uppercase letters?")
+    //  creates confirm boolean for uppercase "yes or no"
+    var uppercaseCharactersChoice = confirm("How about a few uppercase letters?");
 
     if (uppercaseCharactersChoice) {
-        passwordCharSet += uppercaseCharacters;
+        passwordChar += uppercaseChar;
     }
-
+    //  creates confirm boolean for lowercase "yes or no"
     var numericalCharactersChoice = confirm("What’s a password without a couple of numbers? Would you like to add them to yours?");
 
-    if (specialCharacterChoice) {
-        passwordCharSet += specialCharacters;
+    if (numericalCharactersChoice) {
+        passwordChar += numericalChar;
     }
-    
+    //  creates confirm boolean for special characters "yes or no"
+    var specialCharacterChoice = confirm("Your password is pretty secure already, but how about a dash of special characters?");
+
+    if (specialCharacterChoice) {
+        passwordChar += specialChar;
+    }
+
+    for (var i = 0; i < passwordLengthUser; i++) {
+        var newPw = passwordChar[Math.floor(Math.random() * passwordChar.length)]
+    }
+
+    function writePassword() {
+        var newPw = generatePassword();
+        var pwTextArea = document.querySelector("#password")
+        pwTextArea.value = newPw;
+    }
 }
+
+generateBtn.addEventListener("click", generatePassword);
+writePassword();
